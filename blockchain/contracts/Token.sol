a2ee110adee2fffbe120b817ab0d27214130434c
Patrick <patrick.suehrer@thedac.info>
2021-10-04 08:19:35 +0200

    added tests script

--- blockchain/contracts/Token.sol
+++ blockchain/contracts/Token.sol
@@ -5,4 +5,11 @@ pragma solidity ^0.5.0;
 contract Token {
     // variables
     string public name = "Blue Ocean";
+    string public symbol = "BLUE";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10**decimals);
+    }
 }
